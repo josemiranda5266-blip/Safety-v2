8995fe61fe77c065bd8b70788a781eb3e99b9dba
josemiranda5266-blip <josemiranda5266@gmail.com>
2026-08-29 12:25:21 -0300

    feat(hygiene): register protected v2 hygiene routes

--- server.ts
+++ server.ts
@@ -15,6 +15,7 @@ import employeeRoutes from "./server/routes/employeeRoutes";
 import capaRoutes from "./server/routes/capaRoutes";
 import documentRoutes from "./server/routes/documentRoutes";
 import tenantContextRoutes from "./server/routes/tenantContextRoutes";
+import hygieneRoutes from "./server/routes/hygieneRoutes";
 import {
   initializeAuthorizationRepository,
   getAuthorizationRepository,
@@ -153,6 +154,7 @@ app.use("/api/v2/employees", employeeRoutes);
 app.use("/api/v2/capa", capaRoutes);
 app.use("/api/v2/documents", documentRoutes);
 app.use("/api/v2/tenant", tenantContextRoutes);
+app.use("/api/v2/hygiene", hygieneRoutes);
 
 // Global API Error Handler (Ensures all Express /api errors return JSON, never HTML)
 app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
